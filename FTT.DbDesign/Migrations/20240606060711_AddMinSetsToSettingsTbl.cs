701b3c29d3459879a8d820d81a244e9717bae73d
Marian Cojocaru <cojocarumarianstefan@gmail.com>
2024-06-06 10:22:43 +0300

    Migration

---
+++ FTT.DbDesign/Migrations/20240606060711_AddMinSetsToSettingsTbl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FTT.DbDesign.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMinSetsToSettingsTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MinSets",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinSets",
+                table: "Settings");
+        }
+    }
+}
---
+++ FTT/ViewModels/RulesResultViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FTT.ViewModels
+{
+    public class RulesResultViewModel
+    {
+        public int FailCount { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+}
